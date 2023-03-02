1167d3bec03cd794b527b46e8e928b16ac1b3e42
Sebastian Masters <sebastian_masters@hotmail.com>
2023-03-02 17:44:04 -0300

    first commit

---
+++ PrimerProyecto/PrimerProyecto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace PrimerProyecto
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var objeto1 = new Program();
+
+            objeto1.rellenar();
+        }
+        public void rellenar()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            var nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            var edad = Console.ReadLine();
+
+            Console.WriteLine($"{nombre}  {edad}");
+            Console.ReadLine();
+        }
+    }
+}
